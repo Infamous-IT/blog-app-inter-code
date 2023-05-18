cc2f69dab2cdbb27e92f5fc6e2ec2d3ce345a398
infamous-it <gluhanuk98@gmail.com>
2023-05-18 13:57:05 +0300

    Added functionality for active category in menu bar and change code in post component - added if statement for checked active category

--- client/src/app/service/post.service.ts
+++ client/src/app/service/post.service.ts
@@ -34,8 +34,13 @@ export class PostService {
     return this.http.delete<void>(`${this.baseUrl}/${id}`);
   }
 
-  searchPosts(query: any): Observable<Post[]> {
-    return this.http.get<Post[]>(`${this.baseUrl}/search/by`, { params: query });
+  // searchPosts(query: any): Observable<Post[]> {
+  //   return this.http.get<Post[]>(`${this.baseUrl}/search/by`, { params: query });
+  // }
+
+  searchPosts(category: string): Observable<Post[]> {
+    const params = { category };
+    return this.http.get<Post[]>(`${this.baseUrl}/search/by`, { params });
   }
 
   sortByCreationDate(sortOrder: string): Observable<Post[]> {
